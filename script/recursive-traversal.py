7202c1d9dabbe3f1ad71c1e7747d9bb1ccf33e6f
nielwyn <nghoangnghiabt@gmail.com>
2025-04-11 00:00:16 +0700

    update snippet

--- script/recursive-traversal.py
+++ script/recursive-traversal.py
@@ -40,5 +40,35 @@ def recursive_for_brave_string():
                         for brave_string in brave_string_list:
                             if brave_string in brave_herond_string_dictionary:
                                 replace_brave_to_herond(os.path.join(root, file), brave_string, brave_herond_string_dictionary[brave_string])
-recursive_for_brave_string()
 
+def remove_duplicate():
+    with open('./chromium_src/herond/components/herond_wallet/browser/herond_wallet_constants.h', 'r') as read_file:
+        array = list()
+        id_array = list()
+        lines = read_file.readlines()
+        for line in lines:
+            match = re.findall(r"herondWallet\w*", line)
+            id_match = re.findall(r"IDS_HEROND_WALLET_\w*", line)
+            # print(match)
+            if match: array.append(match[0])
+            if id_match: id_array.append(id_match[0])
+            # print(array)
+        dup_list = [item for item, count in collections.Counter(array).items() if count > 1]
+        id_dup_list = [item for item, count in collections.Counter(id_array).items() if count > 1]
+        # print(id_dup_list)
+
+        with open('./chromium_src/herond/components/herond_wallet/browser/herond_wallet_constants_test.h', 'w') as write_file:
+            commented = list() 
+            id_commented = list() 
+            for line in lines:
+                match = re.findall(r"herondWallet\w*", line)
+                id_match = re.findall(r"IDS_HEROND_WALLET_\w*", line)
+                # print(id_match)kjj
+                if match and match[0] not in commented:
+                    write_file.writelines(line)
+                    commented.append(match[0])
+                    # if id_match: id_commented.append(id_match[0])
+                
+                if id_match and id_match[0] not in id_commented and not match:
+                    write_file.writelines(line)
+                    id_commented.append(id_match[0])
